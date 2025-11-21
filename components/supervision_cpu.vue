bea5117727288e44cdf4f86f2c267080b6347c90
Soledine <soledine22@gmail.com>
2025-11-21 13:35:52 +0100

    commit initial

---
+++ components/supervision_cpu.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { ref, onMounted} from 'vue';
+  const cpuLoad = ref([]);
+  import axios from "axios";
+
+
+   onMounted(() => {
+      axios.get("http://localhost:8080/api/mesuresCpu").then(reponse => {
+      // ajouter les nouvelles valeurs
+      reponse.data.forEach(val => cpuLoad.value.push(val))
+
+      //console.log("CPU Load :", cpuLoad)
+    })
+    .catch(err => console.error(err))
+  });
+
+
+</script>
+<template><p>charge Cpu: </p>
+    <p>{{cpuLoad}}</p>
+
+
+    <!--
+      <ul>
+        <li v-for="(load, index) in cpuLoad" :key="index">
+          {{ load }}
+        </li>
+      </ul>
+    -->
+</template>
+
