6b702579f32b6767844919775ea7f77d1bc1680d
Soledine <soledine22@gmail.com>
2025-11-23 09:12:47 +0100

    nettoyage du code

--- components/supervision_cpu.vue
+++ components/supervision_cpu.vue
@@ -3,19 +3,10 @@ import { ref, onMounted, nextTick } from 'vue'
 import axios from 'axios'
 import { Chart } from 'chart.js/auto'
 
-const cpuLoad = ref([])
 const chartCanvas = ref(null)
 
 onMounted(async () => {
   try {
-    const response = await axios.get("http://localhost:8080/api/mesuresCpu")
-
-    // 🔥 EXTRACTION DE cpuLoad
-    cpuLoad.value = response.data.map(item => item.cpuLoad)
-
-    console.log("Valeurs CPU utilisées :", cpuLoad.value)
-
-    await nextTick()
 
     let chartInstance = new Chart(chartCanvas.value.getContext('2d'), {
       type: 'line',
@@ -56,14 +47,11 @@ onMounted(async () => {
       let mesureCpu = reponse.data.cpuLoad
       ajouterValeur(mesureCpu);
       console.log(mesureCpu)
-      await sleep(500);
+      await sleep(3000);
       }
     }
 
     completerGrapheCpu();
-
-
-
   } catch (error) {
     console.error("Erreur API :", error)
   }
