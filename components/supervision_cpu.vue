ae79e30056b01edf5068885df9bad98cd0b48c38
Soledine <soledine22@gmail.com>
2025-11-30 12:34:55 +0100

    FEAT: ajout d'un bloc d'affichage statistique

--- components/supervision_cpu.vue
+++ components/supervision_cpu.vue
@@ -4,6 +4,10 @@ import axios from 'axios'
 import { Chart } from 'chart.js/auto'
 
 const chartCanvas = ref(null)
+let currentValue=ref(0)
+let minValue=ref(0)
+let maxValue=ref(0)
+
 
 onMounted(async () => {
   try {
@@ -30,10 +34,14 @@ onMounted(async () => {
       }
     })
 
-    function ajouterValeur(nouvelleValeur) {
-      const index = chartInstance.data.labels.length + 1
-      chartInstance.data.labels.push(`Mesure ${index}`)
+    function ajouterValeur(nouvelleValeur,time) {
+      //const index = chartInstance.data.labels.length + 1
+      chartInstance.data.labels.push(time)
       chartInstance.data.datasets[0].data.push(nouvelleValeur)
+      currentValue.value=nouvelleValeur
+      console.log(chartInstance.data.datasets[0].data)
+      minValue.value=Math.min(...chartInstance.data.datasets[0].data)
+      maxValue.value=Math.max(...chartInstance.data.datasets[0].data)
       chartInstance.update()
     }
 
@@ -46,7 +54,8 @@ onMounted(async () => {
       //let reponse = await axios.get("http://localhost:8080/api/lastMesure")
       let reponse = await axios.get("/api/lastMesure")
       let mesureCpu = reponse.data.cpuLoad
-      ajouterValeur(mesureCpu);
+      let time = reponse.data.time
+      ajouterValeur(mesureCpu,time);
       console.log(mesureCpu)
       await sleep(3000);
       }
@@ -60,7 +69,30 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div style="height:300px;">
-    <canvas ref="chartCanvas"></canvas>
+  <div class="cpu-container">
+    <!-- Graphe -->
+    <div class="chart-wrapper">
+      <canvas ref="chartCanvas"></canvas>
+    </div>
+
+    <!-- Bloc statistiques -->
+    <div class="stats-wrapper">
+      <h4>CPU (%)</h4>
+
+      <div class="stat">
+        <span class="label">Courant</span>
+        <span class="value current">{{ currentValue }} %</span>
+      </div>
+
+      <div class="stat">
+        <span class="label">Min</span>
+        <span class="value">{{ minValue }} %</span>
+      </div>
+
+      <div class="stat">
+        <span class="label">Max</span>
+        <span class="value">{{ maxValue }} %</span>
+      </div>
+    </div>
   </div>
 </template>
